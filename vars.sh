09275ab9931e12e6d864ca66e34eb6181b4f00c3
Vijitha3111 <vijithaviji1108@gmail.com>
2024-05-06 17:47:58 +0100

    variable script

---
+++ vars.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "please enter username:"
+
+read USERNAME 
+
+echo "please enter the password"
+
+read PASSWORD 
+
+echo " Username is $USERNAME and Password is $PASSWORD"
+
+
+
+
